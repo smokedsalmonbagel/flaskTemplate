36d802fddbb0b41e924665df9f509226ac3d109e
Tyler <conlontj@clarkson.edu>
2020-03-03 11:01:30 -0500

    first version

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from flask import Flask
 from flask import render_template
-app = Flask(__name__)
+from flask import request,session, redirect, url_for, escape,send_from_directory,make_response 
 
+import pymysql 
+import json
+app = Flask(__name__,static_url_path='')
+
+
+@app.route('/basichttp')
+def basichttp():
+    if request.args.get('myvar') is not None:
+        return 'your var:' + request.args.get('myvar')
+    else:
+        return 'myvar not set' 
 
 @app.route('/')
+def home():
+    return render_template('test.html', title='Test', msg='Welcome!')
+
 @app.route('/index')
 def index():
     user = {'username': 'Tyler'}
@@ -14,6 +28,10 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, items=items)
 
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
 
 if __name__ == '__main__':
-   app.run()
+   app.secret_key = '1234'
+   app.run(host='127.0.0.1',debug=True)
